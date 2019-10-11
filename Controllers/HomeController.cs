32847088e3a70ff2e60cb6d4b9b086b3490b5c44
daanneek <daanneek@gmail.com>
2019-10-11 20:48:33 +0200

    I want to die /s

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,37 @@
-using DataLibrary;
+﻿using static DataLibrary.Logic.TeacherProcessor;
+using static DataLibrary.Logic.StudentProcessor;
+
+using ProjectCSA.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using static DataLibrary.Logic.TeacherProcessor;
-
-
-
 namespace ProjectCSA.Controllers
+
 {
     public class HomeController : Controller
     {
         Pwenc penc = new Pwenc();
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Message = "Home page";
+
+            var data = LoadStudents();
+
+            List<StudentModel> student = new List<StudentModel>();
+            foreach (var row in data)
+            {
+                student.Add(new StudentModel
+                {
+                    Snum = row.Snum,
+                    Fname = row.Fname,
+                    Lname = row.Lname,
+                    cnum = row.cnum
+                });
+            }
+
+            return View(student);
         }
 
         public ActionResult About()
@@ -24,39 +40,19 @@ namespace ProjectCSA.Controllers
 
             return View();
         }
-        public ActionResult ViewTeachers()
-        {
-            ViewBag.Message = "Teachers List";
 
-            var data = LoadTeachers();
-            List<TeacherModel> teachers = new List<TeacherModel>();
-
-            foreach (var row in data)
-            {
-                teachers.Add(new TeacherModel
-                {
-                    Tcode = row.Tcode,
-                    Fname = row.Fname,
-                    Infix = row.Infix,
-                    Lname = row.Lname
-                });
-            }
-            return View(teachers);
-        }
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
-
-        public ActionResult SignIn()
+        public ActionResult SignUp()
         {
-            ViewBag.Message = "Teacher Sign in";
+            ViewBag.Message = "Teacher Sign Up";
 
             return View();
         }
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult SignIn(TeacherModel model)
@@ -71,6 +67,26 @@ namespace ProjectCSA.Controllers
                 return RedirectToAction("index");
             }
             return View();
+        }
+
+        public ActionResult ViewTeachers()
+        {
+            ViewBag.Message = "Teacher List";
+
+            var data = LoadTeachers();
+
+            List<TeacherModel> teachers = new List<TeacherModel>();
+            foreach (var row in data)
+            {
+                teachers.Add(new TeacherModel
+                {
+                    Tcode = row.Tcode,
+                    Fname = row.Fname,
+                    Lname = row.Lname,
+                });
+            }
+
+            return View(teachers);
         }
     }
 }
--- Models/StudentModel.cs
+++ Models/StudentModel.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectCSA.Models
 {
     public class StudentModel
     {
-        public int Snum { get; set; }
+
+        public string Snum { get; set; }
+
         public string Fname { get; set; }
-        public string Infix { get; set; }
+
         public string Lname { get; set; }
+
+        public string cnum { get; set; }
+
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+
+        public string confirmPassword { get; set; }
+
+
     }
 }
