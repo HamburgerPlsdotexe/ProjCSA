da4c18cf955559a17820f99e328c2afc8c3a5847
TheDenizen <25197071+TheDenizen@users.noreply.github.com>
2019-11-16 22:58:18 +0100

    dumping this commit so i do not lose all my settings and edits again

---
+++ Views/Application/QRCoder.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QRCoder.aspx.cs" Inherits="ProjectCSA.QRCoder_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="qr_text_input" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="qr_button" runat="server" Text="Generate QR" OnClick="qr_button_Click" />
+            <br />
+            <asp:PlaceHolder ID="PlaceholderQR" runat="server"></asp:PlaceHolder>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Views/Application/QRCoder.aspx.cs
@@ -0,0 +1,46 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectCSA
+{
+    public partial class QRCoder_Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void qr_button_Click(object sender, EventArgs e)
+        {
+            //This variable is the input for the qr-code, which should be pulled from the database instead of being an on-click event
+            string Code = qr_text_input.Text;
+            QRCodeGenerator qrgenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrgenerator.CreateQrCode(Code, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            System.Web.UI.WebControls.Image imgQRcode = new System.Web.UI.WebControls.Image();
+            imgQRcode.Width = 150;
+            imgQRcode.Height = 150;
+
+            using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
+            {
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    qrCodeImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    imgQRcode.ImageUrl = "data:image/png;base64" + Convert.ToBase64String(byteImage);
+                }
+                PlaceholderQR.Controls.Add(imgQRcode);
+            }
+        }
+
+    }
+}
