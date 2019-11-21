e2ccc5ec1b7575d142f0bac63f4ce99afd8a1453
Caglar <caglar_G@live.nl>
2019-11-21 17:30:00 +0100

    Made a beginning with the teacher schedules

---
+++ Models/ScheduleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectCSA.Models
+{
+    public class ScheduleModel
+    {
+        public string Lesson { get; set; }
+        public string Title { get; set; }
+        public string Day { get; set; }
+        public string Hour { get; set; }
+        public string classroom { get; set; }
+    }
+}
